6c34e7809d6b4d92fdd46134ff5467082c4434a8
Denis Usanov <denis@usanov.info>
2022-12-10 16:52:39 +0300

    Render day 10 solution dynamically

--- 2022/10/main.go
+++ 2022/10/main.go
@@ -7,6 +7,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type Command struct {
@@ -15,15 +18,28 @@ type Command struct {
 }
 
 type VM struct {
-	Pixels [][]rune
-	width  int
-	height int
-	x      int
-	cycle  int
+	Pixels   [][]rune
+	width    int
+	height   int
+	x        int
+	cycle    int
+	step     time.Duration
+	tickChan chan<- Refresh
 }
 
-func CreateVM(width int, height int) *VM {
-	vm := &VM{width: width, height: height, x: 1, cycle: 0}
+type Refresh struct {
+	quit bool
+}
+
+func CreateVM(width int, height int, step time.Duration, tickChan chan<- Refresh) *VM {
+	vm := &VM{
+		width:    width,
+		height:   height,
+		x:        1,
+		cycle:    0,
+		step:     step,
+		tickChan: tickChan,
+	}
 
 	vm.Pixels = make([][]rune, vm.height)
 
@@ -45,6 +61,8 @@ func (vm *VM) tick() {
 	}
 
 	vm.cycle++
+	vm.tickChan <- Refresh{}
+	time.Sleep(vm.step)
 }
 
 func (vm *VM) Execute(commands []Command) {
@@ -58,15 +76,58 @@ func (vm *VM) Execute(commands []Command) {
 			vm.x += c.Arg
 		}
 	}
+
+	vm.tickChan <- Refresh{quit: true}
 }
 
-func (vm *VM) String() string {
+func waitForActivity(sub chan Refresh) tea.Cmd {
+	return func() tea.Msg {
+		return <-sub
+	}
+}
+
+type Model struct {
+	Vm       *VM
+	Sub      chan Refresh
+	quitting bool
+}
+
+func (m *Model) Init() tea.Cmd {
+	return waitForActivity(m.Sub)
+}
+
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+		}
+	case Refresh:
+		if msg.quit {
+			return m, tea.Quit
+		} else {
+			return m, waitForActivity(m.Sub)
+		}
+	}
+
+	return m, nil
+}
+
+func (m *Model) View() string {
 	var s strings.Builder
 
-	for i := 0; i < vm.height; i++ {
+	for i := 0; i < m.Vm.height; i++ {
 		fmt.Fprintf(&s, "%d [", i+1)
-		for j := 0; j < vm.width; j++ {
-			fmt.Fprintf(&s, "%c", vm.Pixels[i][j])
+		for j := 0; j < m.Vm.width; j++ {
+
+			if i*m.Vm.width+j == m.Vm.cycle {
+				fmt.Fprintf(&s, "\033[1m%c\033[0m", m.Vm.Pixels[i][j])
+			} else {
+				fmt.Fprintf(&s, "%c", m.Vm.Pixels[i][j])
+			}
+
 		}
 		fmt.Fprint(&s, "]\n")
 	}
@@ -92,7 +153,19 @@ func main() {
 		commands = append(commands, Command{Instruction: instruction, Arg: arg})
 	}
 
-	vm := CreateVM(40, 6)
-	vm.Execute(commands)
-	fmt.Println(vm)
+	refreshChan := make(chan Refresh)
+	vm := CreateVM(40, 6, 20*time.Millisecond, refreshChan)
+
+	p := tea.NewProgram(&Model{
+		Sub: refreshChan,
+		Vm:  vm,
+	})
+
+	go vm.Execute(commands)
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("could not start program:", err)
+		os.Exit(1)
+	}
+
 }
