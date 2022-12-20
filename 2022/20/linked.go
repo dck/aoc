08005f397fb0cc308ca23b4c3f7360f91c897427
Denis Usanov <denis@usanov.info>
2022-12-20 21:59:26 +0300

    Add Linked List based solution to Day 20

---
+++ 2022/20/linked.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Number struct {
+	value    int
+	position int
+	next     *Number
+	prev     *Number
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	encoded := []*Number{}
+	i := 0
+
+	var buf *Number
+	var root *Number
+	for scanner.Scan() {
+		line := scanner.Text()
+		n, _ := strconv.Atoi(line)
+
+		num := &Number{value: n, position: i}
+		encoded = append(encoded, num)
+		if buf == nil {
+			buf = num
+			root = num
+		} else {
+			num.prev = buf
+			buf.next = num
+			buf = num
+		}
+		i++
+	}
+
+	buf.next = root
+	root.prev = buf
+
+	fmt.Println(pretty(root))
+
+	for _, n := range encoded {
+		id := n.position
+		target := root
+		for target.position != id {
+			target = target.next
+		}
+
+		steps := target.value
+
+		if steps < 0 {
+			for i := 0; i > steps; i-- {
+				next := target.next
+				prev := target.prev
+
+				next.prev = prev
+				prev.next = next
+
+				target.next = prev
+				target.prev = prev.prev
+
+				prev.prev.next = target
+				prev.prev = target
+			}
+		} else if steps > 0 {
+			for i := 0; i < steps; i++ {
+				next := target.next
+				prev := target.prev
+
+				prev.next = next
+				next.prev = prev
+
+				target.prev = next
+				target.next = next.next
+
+				next.next = target
+				target.next.prev = target
+			}
+		} else {
+			continue
+		}
+	}
+
+	index := 1
+	p := root.next
+	for p != nil && p != root && p.value != 0 {
+		index++
+		p = p.next
+	}
+	index = index % len(encoded)
+	l := len(encoded)
+	a := getByIndex(root, (index+1000)%l).value
+	b := getByIndex(root, (index+2000)%l).value
+	c := getByIndex(root, (index+3000)%l).value
+	fmt.Println(a + b + c)
+}
+
+func getByIndex(root *Number, index int) *Number {
+	if root == nil {
+		return nil
+	}
+
+	p := root
+	for i := 0; i < index; i++ {
+		p = p.next
+	}
+
+	return p
+}
+
+func pretty(root *Number) string {
+	var s strings.Builder
+
+	if root == nil {
+		return ""
+	}
+
+	p := root
+	fmt.Fprint(&s, "[")
+	fmt.Fprintf(&s, "%d ", p.value)
+	p = p.next
+	for p != nil && p != root {
+		fmt.Fprintf(&s, "%d ", p.value)
+		p = p.next
+	}
+	fmt.Fprint(&s, "]")
+
+	return s.String()
+}
